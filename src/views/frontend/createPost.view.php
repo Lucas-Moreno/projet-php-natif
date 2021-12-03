b28e436b038037072dfbd23fa51bccee9a3abeac
Lucas-Moreno <lucas.moreno@magnitude-labs.com>
2021-12-03 17:29:25 +0100

    add view

---
+++ src/views/frontend/createPost.view.php
@@ -0,0 +1,10 @@
+<pre>
+  <div>
+    <input type="text" id="article-title" class="form-control">
+    <label for="artcile-title">Votre Titre :</label>
+    <input type="text" id="article-content">
+    <label for="artcile-content">Votre Article :</label>
+    <input type="file" id="avatar" name="article-picture" accept="image/png, image/jpeg">
+    <label for="avatar">Photo de l'article :</label>
+  </div>
+</pre>
--- src/views/frontend/home.view.php
+++ src/views/frontend/home.view.php
@@ -1,6 +1,3 @@
 <pre>
-    <?php foreach ($users as $user) :?>
-        <h1><?= $user->getName();?></h1>
-
-    <?php  endforeach; ?>
+    
 </pre>
