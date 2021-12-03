79f1b011e83f4490b69afff5df9f9f668eff8f4e
Lucas-Moreno <lucas.moreno@magnitude-labs.com>
2021-12-03 17:17:44 +0100

    crud without update and delete

--- src/Models/UserManager.php
+++ src/Models/UserManager.php
@@ -12,10 +12,28 @@ class UserManager
     }
 
 
-    public function manageUser() {
+    public function getAllUser() {
         $query = 'SELECT * FROM `user`';
         $response = $this->pdo->query($query);
         return $response->fetchAll(\PDO::FETCH_CLASS, 'App\Entity\User');
+    }
+
+    public function getOneUser(int $id) {
+
+        $sth = $this->pdo->prepare('SELECT * FROM `user` WHERE id = ?');
+        $sth->execute(array($id));
+        return $sth->fetchAll(\PDO::FETCH_CLASS, 'App\Entity\User');
+    }
+
+    public function createUser($name, $password, $email) {
+
+        $query = $this->pdo->prepare('INSERT INTO user (name, password, email) VALUES (:name, :password, :email)');
+
+        $query->bindParam(':name', $name);
+        $query->bindParam(':password', $password);
+        $query->bindParam(':email', $email);
+
+        return $query->execute();
     }
 }
 
