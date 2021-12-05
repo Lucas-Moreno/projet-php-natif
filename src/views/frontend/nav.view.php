bcab461ef82d58b1290ec040a7e8256fe273285c
ccosneau0 <charles.cosneau@hetic.net>
2021-12-05 19:39:31 +0100

    last push

--- src/views/frontend/nav.view.php
+++ src/views/frontend/nav.view.php
@@ -1,9 +1,4 @@
 <nav class="navbar navbar-light bg-light">
-  <a class="navbar-brand" href="#">HOME</a>
-  <a class="navbar-brand" href="#">Write Article</a>
-  <a class="navbar-brand" href="#">Post API</a>
-  <a class="navbar-brand" href="#">Comments API</a>
-  <?php if($is_admin === true): ?>
-      <a class="navbar-brand" href="#">User List</a>
-  <?php endif; ?>
+  <a class="navbar-brand" href="http://localhost:5555/home">HOME</a>
+  <a class="navbar-brand" href="http://localhost:5555/userlist">User List</a>
 </nav>
